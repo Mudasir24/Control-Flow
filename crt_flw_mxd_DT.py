cdafc48913ac5ad7a651ea99ce39eeb7ed184ddb
Mudasir24 <151111505+Mudasir24@users.noreply.github.com>
2024-01-13 16:22:07 +0530

    Add files via upload

---
+++ crt_flw_mxd_DT.py
@@ -0,0 +1,6 @@
+# Declaring a list
+mixed_data = [85,"Mudasir","Mango",15.84,"India",3+2j,"MSS",12,True,3.14159]
+
+# printing each element along with its data type using for loop
+for i in range (0,len(mixed_data)):
+    print("The data type of ",mixed_data[i]," is ",type(mixed_data[i]))
